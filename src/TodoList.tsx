bcdf9dad4da9c7945c88d87b5f30c25826ac03d5
Anna-er <brungilda364@gmail.com>
2023-10-12 17:07:13 +0300

    fix: fix TodoList bugs and code style

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,7 +1,4 @@
-
-import { useState } from "react";
-import { useEffect } from "react";
-import * as React from 'react';
+import { useState, useEffect, FC } from 'react';
 
 
 const TODO_LIST_ENDPOINT = 'http://localhost:3000/todos/'
@@ -17,7 +14,7 @@ interface item {
 export const TodoList: React.FC = () => {
     const [todos, setTodos] = useState<item[]>([]);
 
-    const [input, setInput] = useState<string>("");
+    const [newTodoText, setNewTodoText] = useState("");
 
     useEffect(() => {
         fetch(TODO_LIST_ENDPOINT)
@@ -50,32 +47,30 @@ export const TodoList: React.FC = () => {
 
 
     const handleClick = () => {
-        if (input != "" && input.split(' ').join('') != "") {
+        if (newTodoText.trim() !== "") {
             fetch(TODO_LIST_ENDPOINT, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ text: input, completed: false })
+                body: JSON.stringify({ text: newTodoText, completed: false })
             }).then(
                 response => response.json()
             ).then(
                 data => setTodos(todos.concat(data))
             );
 
-            setInput("");
+            setNewTodoText("");
         }
-        (document.getElementById("inp") as HTMLInputElement).value = "";
     };
 
 
     const handleDelete = (id: number) => {
-        fetch(TODO_LIST_ENDPOINT + id.toString(), {
+        fetch(`${TODO_LIST_ENDPOINT}/${id}`, {
             method: 'DELETE'
         }).then(r => {
             if (r.ok) {
                 const newTodos = todos.filter(todo => todo.id !== id);
-                console.log(newTodos);
                 setTodos(newTodos);
             }
         })
@@ -88,20 +83,19 @@ export const TodoList: React.FC = () => {
             <h1>Todo List</h1>
             <div className="list">
                 {todos.map((todo) => (
-                    <div className="items"
+                    <div className="item"
                         key={todo.id}
-                        onClick={() => handleToggle(todo.id)}
                         style={{ textDecoration: todo.completed ? "line-through" : "none", color: todo.completed ? "#8E8E8E" : "#000000" }}
                     >
-                        <input type="checkbox" style={{ margin: '0.4rem' }} name="todo" id="item" value={todo.id} checked={todo.completed} readOnly />
-                        <label id="item">
+                        <input type="checkbox" onClick={() => handleToggle(todo.id)} className='checkbox' name="todo" id={todo.id.toString()} checked={todo.completed} readOnly />
+                        <label htmlFor={todo.id.toString()}>
                             {todo.text}
                         </label>
                         <button className='remove-button' onClick={() => handleDelete(todo.id)}>×</button>
                     </div>
                 ))}
             </div>
-            <input className="input-text" type="text" id="inp" placeholder="Add todo item" onChange={(e) => setInput(e.currentTarget.value)} />
+            <input className="input-text" type="text" value={newTodoText} placeholder="Add todo item" onChange={(e) => setNewTodoText(e.currentTarget.value)} />
             <button onClick={handleClick}>Add</button>
         </div>
     );
